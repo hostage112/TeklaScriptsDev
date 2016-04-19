ab46c071868a22d495c066fddaedbeaf5fbe3847
hostage112 <ess.aleksandr@gmail.com>
2016-04-19 23:44:00 +0300

    changes

---
+++ 1_jooniste_nimekiri.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Reflection;
+using System.Collections;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Tekla.Structures;
+using Tekla.Structures.Drawing;
+using TSD = Tekla.Structures.Drawing;
+using Tekla.Structures.Drawing.UI;
+using Tekla.Structures.Model;
+using Tekla.Structures.Geometry3d;
+
+using System.IO;
+using System.Text;
+
+namespace Tekla.Technology.Akit.UserScript
+{
+    public class Script
+    {
+
+        public static void Run(Tekla.Technology.Akit.IScript akit)
+        {
+            string fileName = @"test.txt";
+
+            try
+            {
+                if (File.Exists(fileName))
+                {
+                    File.Delete(fileName);
+                }
+            }
+            catch { }
+
+
+            int drawingNr = 1;
+            DrawingHandler myDrawingHandler = new DrawingHandler();
+            DrawingEnumerator selectedDrawings = myDrawingHandler.GetDrawingSelector().GetSelected();
+
+            var csv = new StringBuilder();
+
+            foreach (Drawing currentDrawing in selectedDrawings)
+            {
+                string name = currentDrawing.Title1;
+                string nr = currentDrawing.Name;
+                string gg = "";
+                currentDrawing.GetUserProperty("REVISION.LAST_NUMBER", ref gg);
+                string newLine = string.Format("{0};{1};{2}", name, nr, gg);
+                csv.AppendLine(newLine);
+                string mark = "";
+                DateLastMark(currentDrawing, ref mark);
+                drawingNr++;
+                MessageBox.Show(newLine + mark);
+            }
+
+
+            //File.WriteAllText(fileName, csv.ToString());
+
+            if (File.Exists(fileName))
+            {
+                Process.Start(fileName);
+            }
+            
+        }
+
+        public static string DateLastMark(Drawing croquis, out string mark)
+        {
+            DrawingHandler drawingHandler = new DrawingHandler();
+            Type drawingType = croquis.GetType();
+            PropertyInfo propertyInfo = drawingType.GetProperty("Identifier", BindingFlags.Instance | BindingFlags.NonPublic);
+            object value = propertyInfo.GetValue(croquis, null);
+
+            Identifier identifier = (Identifier)value;
+            Beam fakeBeam = new Beam { Identifier = identifier };
+            string RevisionMark = "";
+            fakeBeam.GetReportProperty("REVISION.LAST_MARK", ref RevisionMark);
+
+        }
+    }
+}
