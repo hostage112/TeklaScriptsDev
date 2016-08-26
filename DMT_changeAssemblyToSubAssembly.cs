d77d4f8bfbeac4220360e96639453cd21032b9be
hostage112 <ess.aleksandr@gmail.com>
2016-08-26 16:10:25 +0300

    gg

--- DMT_changeAssemblyToSubAssembly.cs
+++ DMT_changeAssemblyToSubAssembly.cs
@@ -25,13 +25,25 @@ namespace Tekla.Technology.Akit.UserScript
             int wrongPartsCount = 0;
 
             TSM.ModelObjectEnumerator selectedObjects = getSelectedObjects();
-            List<Assembly> selectedAssemblys = getSelectedAssemblys(selectedObjects);
+            List<TSM.Assembly> selectedAssemblys = getSelectedAssemblys(selectedObjects);
 
             foreach (TSM.Assembly currentAssembly in selectedAssemblys)
             {
                 List<TSM.Part> wrongParts = findWrongParts(currentAssembly);
-                fixWrongParts(currentAssembly, wrongParts, akit);
-                wrongPartsCount += wrongParts.Count;
+
+				if (wrongParts.Count > 0)
+				{                
+					fixWrongParts(currentAssembly, wrongParts, akit);
+					
+					TSM.Part mainpart = currentAssembly.GetMainPart() as TSM.Part;
+					if (currentAssembly.Name == mainpart.Name)
+					{
+						removeAssemblyPropertys(currentAssembly, akit);
+					}
+				}          
+				
+				wrongPartsCount += wrongParts.Count;
+				
             }
 
             MessageBox.Show("Kontrollitud " + selectedAssemblys.Count.ToString() + " assembly." + Environment.NewLine +
@@ -48,13 +60,13 @@ namespace Tekla.Technology.Akit.UserScript
 
         private static List<TSM.Assembly> getSelectedAssemblys(TSM.ModelObjectEnumerator selectedObjects)
         {
-            List<Assembly> selectedAssemblys = new List<Assembly>();
+            List<TSM.Assembly> selectedAssemblys = new List<TSM.Assembly>();
 
             while (selectedObjects.MoveNext())
             {
-                if (selectedObjects.Current is Assembly)
+                if (selectedObjects.Current is TSM.Assembly)
                 {
-                    selectedAssemblys.Add(selectedObjects.Current as Assembly);
+                    selectedAssemblys.Add(selectedObjects.Current as TSM.Assembly);
                 }
             }
 
@@ -82,66 +94,63 @@ namespace Tekla.Technology.Akit.UserScript
         {
             foreach (TSM.Part part in wrongParts)
             {
-                changePartToSubAssembly(part, assembly);
+                fixPart(part, assembly);
+                removeAssemblyPropertys(part.GetAssembly() as TSM.Assembly, akit);
             }
         }
 
-        private static void changePartToSubAssembly(TSM.Part part, TSM.Assembly assembly, Tekla.Technology.Akit.IScript akit)
+        private static ArrayList fixPart(TSM.Part part, TSM.Assembly assembly)
         {
+            ArrayList removedAssemblys = new ArrayList();
+
             assembly.Remove(part);
             assembly.Modify();
-            //part.Modify();
 
-            Assembly partNewAssembly = part.GetAssembly() as Assembly;
-            removeAssemblyPropertys(assembly, akit);
+            TSM.Assembly removed = part.GetAssembly() as TSM.Assembly;
 
-            assembly.Add(partNewAssembly);
+            assembly.Add(removed);
             assembly.Modify();
 
-            TSM.Part mainpart = assembly.GetMainPart() as TSM.Part;
-            if (assembly.Name == mainpart.Name)
-            {
-                removeAssemblyPropertys(assembly, akit);
-            }
+            return removedAssemblys;
         }
-    }
 
-    private static void removeAssemblyPropertys(TSM.Assembly currentAssembly, Tekla.Technology.Akit.IScript akit)
-    {
-        ArrayList currentSelection = new ArrayList();
-        currentSelection.Add(currentAssembly);
-
-        TSM.UI.ModelObjectSelector selector = new TSM.UI.ModelObjectSelector();
-        selector.Select(currentSelection);
-
-        akit.Callback("acmd_display_selected_object_dialog", "", "View_01 window_1");
-        TSM.Part currentMainPart = currentAssembly.GetMainPart() as TSM.Part;
-
-        if (!currentMainPart.Material.MaterialString.StartsWith("C"))
+        private static void removeAssemblyPropertys(TSM.Assembly currentAssembly, Tekla.Technology.Akit.IScript akit)
         {
-            akit.ValueChange("steelassembly_1", "AssemblyPrefix", "");
-            akit.ValueChange("steelassembly_1", "AssemblyStartNumber", "");
-            akit.ValueChange("steelassembly_1", "AssemblyName", "");
-            akit.PushButton("modify_button", "steelassembly_1");
-            akit.PushButton("OK_button", "steelassembly_1");
-        }
-        else
-        {
-            if (currentMainPart.CastUnitType == 0)
+            ArrayList currentSelection = new ArrayList();
+            currentSelection.Add(currentAssembly);
+
+            TSM.UI.ModelObjectSelector selector = new TSM.UI.ModelObjectSelector();
+            selector.Select(currentSelection);
+
+            akit.Callback("acmd_display_selected_object_dialog", "", "View_01 window_1");
+            TSM.Part currentMainPart = currentAssembly.GetMainPart() as TSM.Part;
+
+            if (!currentMainPart.Material.MaterialString.StartsWith("C"))
             {
-                akit.ValueChange("precastassembly_1", "AssemblyPrefix", "");
-                akit.ValueChange("precastassembly_1", "AssemblyStartNumber", "");
-                akit.ValueChange("precastassembly_1", "AssemblyName", "");
-                akit.PushButton("modify_button", "precastassembly_1");
-                akit.PushButton("OK_button", "precastassembly_1");
+                akit.ValueChange("steelassembly_1", "AssemblyPrefix", "");
+                akit.ValueChange("steelassembly_1", "AssemblyStartNumber", "");
+                akit.ValueChange("steelassembly_1", "AssemblyName", "");
+                akit.PushButton("modify_button", "steelassembly_1");
+                akit.PushButton("OK_button", "steelassembly_1");
             }
             else
             {
-                akit.ValueChange("insituassembly_1", "AssemblyPrefix", "");
-                akit.ValueChange("insituassembly_1", "AssemblyStartNumber", "");
-                akit.ValueChange("insituassembly_1", "AssemblyName", "");
-                akit.PushButton("modify_button", "insituassembly_1");
-                akit.PushButton("OK_button", "insituassembly_1");
+                if (currentMainPart.CastUnitType == 0)
+                {
+                    akit.ValueChange("precastassembly_1", "AssemblyPrefix", "");
+                    akit.ValueChange("precastassembly_1", "AssemblyStartNumber", "");
+                    akit.ValueChange("precastassembly_1", "AssemblyName", "");
+                    akit.PushButton("modify_button", "precastassembly_1");
+                    akit.PushButton("OK_button", "precastassembly_1");
+                }
+                else
+                {
+                    akit.ValueChange("insituassembly_1", "AssemblyPrefix", "");
+                    akit.ValueChange("insituassembly_1", "AssemblyStartNumber", "");
+                    akit.ValueChange("insituassembly_1", "AssemblyName", "");
+                    akit.PushButton("modify_button", "insituassembly_1");
+                    akit.PushButton("OK_button", "insituassembly_1");
+                }
             }
         }
     }
